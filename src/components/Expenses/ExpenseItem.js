ef4c9a8ac766529d98687e56b153398062ca7eea
bhanu <udaibhanu2602@gmail.com>
2022-09-17 11:12:35 +0530

    Adding Event Listener(edit and delete button) TAsk-2)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,16 @@ import Card from "../UI/Card";
 import ExpenseDetails from "./ExpenseDetails.js";
 import ExpenseDate from "./ExpenseDate";
 let ExpenseItem=(prop)=> {
+
+  const clickHandler=()=>
+  {
+    console.log('clicked!!')
+  }
+  const deleteHandeler=()=>
+  {
+    console.log("deleted")
+  }
+
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
@@ -11,6 +21,8 @@ let ExpenseItem=(prop)=> {
         <h2>{prop.location}</h2>
         <ExpenseDetails  price={prop.price} />
       </div>
+      <button onClick={clickHandler} >Change Title</button>
+      <button onClick={deleteHandeler}>Delete</button>
     </Card>
   );
 }
@@ -20,3 +32,7 @@ export default ExpenseItem;
 //Here in <ExpenseDate> we are using date attribute i.e date={prop.date} it has date in it which is in dynamic values
 //i.e in items array has object with date, it will store that date
 // this attribute will pass as argument to ExpenseDate component function
+
+
+//In react events are passed as "prop" , but "on" should be used before events name, then only react will take it as event (e.g  onClick)
+//here functions that we will passed in events should not be evoked i.e '( )' should not used, other wise they will evoked when page will loaded 
