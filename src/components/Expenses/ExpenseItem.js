3b411a2033a75190d7ad65af421a7937926ecb64
bhanu <udaibhanu2602@gmail.com>
2022-09-17 13:30:53 +0530

    Using useSate function Task-3

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDetails from "./ExpenseDetails.js";
 import ExpenseDate from "./ExpenseDate";
+
 let ExpenseItem = (prop) => {
-  let updateTitle = prop.title;
+ 
+  const[title,setTitle]=useState(prop.title)
 
   const clickHandler = () => {
-    updateTitle = "drinks";
-    console.log(updateTitle)
+    setTitle('updated!!')
+    console.log(title)
   };
   const deleteHandeler = () => {
     console.log("deleted");
@@ -19,7 +21,7 @@ let ExpenseItem = (prop) => {
     <Card className="expense-item">
       <div className="expense-item__description">
         <ExpenseDate date={prop.date} />
-        <h2>{updateTitle} </h2>
+        <h2>{title} </h2>
         <h2>{prop.location}</h2>
         <ExpenseDetails price={prop.price} />
       </div>
@@ -37,3 +39,15 @@ export default ExpenseItem;
 
 //In react events are passed as "prop" , but "on" should be used before events name, then only react will take it as event (e.g  onClick)
 //here functions that we will passed in events should not be evoked i.e '( )' should not used, other wise they will evoked when page will loaded
+
+
+//for any dynamic change in react we need state function from react library
+//This state function is imported and should we write as "useState", 'use' should be used in front of state
+//this useState is only used inside component function and not outside or in nested component function
+//useState return array of two values, first index have to change , and second index changed value
+//therefor we take array to to refrence those values (i.e )  const[title,setTitle]
+//In clickHandler function we se setTitle('input'), having new input value
+//here we are calling useState(), this will change initial value of title to updated
+// and it reload whole component and all jsx code inside it to make it visible on screen
+
+//console.log(title): will show old value because useSate(), schedule function and will appear after some time
