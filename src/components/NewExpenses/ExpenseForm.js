af26a997391f945d8c4e42648255c26d17b5bbcc
bhanu <udaibhanu2602@gmail.com>
2022-09-18 22:46:41 +0530

    Empty form after submitting Task-8(1)

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -34,20 +34,28 @@ function ExpenseForm() {
         console.log(obj[key])
       }
     })
-    
+    newSetTitle('')
+    newSetAmount('')
+    newSetDate('')
+
+    //here we are envoking the useState again with empty input value, which means entered values will become empty
+    // these should be envoked inside the submitHandler, which we are using to submit the form
+    //here the flow will be like we enter new value, stored in state, we submit form and values get console
+    //after that code will reach line 37,38,38 and evoke useState again, it will rerender the component and pass empty values, which will get appear in ui because of "value" keyword we use in each <input>
   }
+  
 
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div>
           <label htmlFor="expense_title">Expense Title</label>
-          <input type="text" id="expense_title" onChange={newTitle} />
+          <input type="text" id="expense_title" value={enteredTitle} onChange={newTitle} />
         </div>
 
         <div className="new-expense__controls">
           <label htmlFor="expense_amount">Expense Amount</label>
-          <input type="number" id="expense_amount" onChange={newAmount} />
+          <input type="number" id="expense_amount" value={enteredAmount} onChange={newAmount} />
         </div>
 
         <div className="new-expense__controls">
@@ -57,6 +65,7 @@ function ExpenseForm() {
             id="expense_date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={newDate}
           />
         </div>
@@ -70,28 +79,7 @@ function ExpenseForm() {
 
 export default ExpenseForm;
 
-// in line 6 we are using vanilla js, basically plain js
-//in js we do like, document.getElementById().addEventListener('onclick', (event)=>{})
-//or
-//document.getElementById().addEventListener("onClick",_click)
-// function _click(event)
-// {
-// statements
-// }
-// to create events
 
-//here we are doing same things in
-// function newInput(event)
-//   {
-//     console.log(event.target.value)
-//   }
-
-//function newInput is on input type text field and here we are targetting its value here
-//i.e event.target.value
-
-// onChange event will fire when you change something
-
-// Here we can use multiple useStates()
-// Note that event will always return string, if it is number as in amount , the number will be in string
-// therefor using ' ' usestate('')
-// imitially it will be empty because form will be empty
+// NOTE:
+// In <form> tag if you use button and add event click on it, it will set request to server and reload the page, this happens only in case of <form> and not other tag
+// therefor we use event.preventDefault() to prevent it from submit 
