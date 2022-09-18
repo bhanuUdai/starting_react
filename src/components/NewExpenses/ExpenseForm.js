5dd2e1c98373719689b959e707e21ee5aed53913
bhanu <udaibhanu2602@gmail.com>
2022-09-18 19:41:40 +0530

    Submitting the from Task-7

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -2,65 +2,52 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
-  // const[enteredTitle,newSetTitle]=useState('')
-  // const[enteredAmount,newSetAmount]=useState('')
-  // const[enteredDate,newSetDate]=useState('')
-
-  //Here we are basically putting all useState in single object
-  //object is created in useState, having initial value(e.g : enteredTitle) which is initially empty, similarly with other input values
-  //userInput will be enteredTitle (i.e old one) and setUserInput will be new one which will be return
-
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, newSetTitle] = useState("");
+  const [enteredAmount, newSetAmount] = useState("");
+  const [enteredDate, newSetDate] = useState("");
 
   function newTitle(event) {
-    // newSetTitle(event.target.value)
-
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle:event.target.value
-    // })
-
-    // why we dont use this setUserInput({ ...userInput, title: event.target.value})
-    // Because as we know that useState( ) form a call back function,
-    // now in this case if you have many previous state, or current state depends upon many previous state, then methods can give you wrong snap shot ot previous state
-    // Therefor we use using function now, having previousState as argument, which will have all previous states as whole therefor data will be correct
-
-    setUserInput((previousInput) => {
-      return { ...previousInput, enteredTitle: event.target.value };
-    });
+    newSetTitle(event.target.value);
   }
 
-  //here previousInput contains all previous state and new state is formed with updated previous states
-
   function newAmount(event) {
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    newSetAmount(event.target.value);
   }
 
   function newDate(event) {
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    newSetDate(event.target.value);
+  }
+
+  function submitHandler(event)
+  {
+    event.preventDefault()
+    let obj={
+      enteredTitle,
+      enteredAmount,
+      enteredDate
+    }
+    let data=Object.keys(obj)
+    data.forEach((key)=>
+    {
+      if(obj[key])
+      {
+        console.log(obj[key])
+      }
+    })
+    
   }
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div>
           <label htmlFor="expense_title">Expense Title</label>
-          <input type="text" id="expense_title" onChange={newTitle}></input>
+          <input type="text" id="expense_title" onChange={newTitle} />
         </div>
 
         <div className="new-expense__controls">
           <label htmlFor="expense_amount">Expense Amount</label>
-          <input type="number" id="expense_amount" onChange={newAmount}></input>
+          <input type="number" id="expense_amount" onChange={newAmount} />
         </div>
 
         <div className="new-expense__controls">
@@ -71,12 +58,11 @@ function ExpenseForm() {
             min="2019-01-01"
             max="2022-12-31"
             onChange={newDate}
-          ></input>
+          />
         </div>
-
-        <div className="new-expense__actions">
-          <button>Submit</button>
-        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
