2df65bcfd173700fbf02ec6af54d6b3409d593f0
bhanu <udaibhanu2602@gmail.com>
2022-09-20 21:57:03 +0530

    Condition Rendering (Ternary And Abuse)Task-12

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,50 +1,86 @@
-import React,{useState} from 'react'
-import ExpensesFilter from '../NewExpenses/ExpensesFilter';
+import React, { useState } from "react";
+import ExpensesFilter from "../NewExpenses/ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expense.css";
-import Card from '../UI/Card.js'
-let Expense=(prop)=> {
-  // let item = prop.items;
+import Card from "../UI/Card.js";
+let Expense = (prop) => {
+  const [filteredYear, setYear] = useState(2020);
 
-  const[filteredYear,setYear]=useState(2020)
-  
-  function getFilterYear(newYear)
-  {
-    setYear(newYear)
+  function getFilterYear(newYear) {
+    setYear(newYear);
   }
 
-  //  console.log(item)
-   let filteredExpenses=prop.items.filter((expense)=>
-   {
-    // console.log(expense.date.getFullYear().toString())
-    // console.log(filteredYear.toString())
 
-      return (expense.date.getFullYear().toString()===filteredYear)
-      
-   })
-   
-console.log(filteredExpenses)
+  
+  let filteredExpenses = prop.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  // console.log(filteredExpenses);
+
+  let expenseContent=<p>No Expense Available</p>
+  if(filteredExpenses.length===1)
+  {
+    expenseContent= <p>Only single Expense here. Please add more...</p>
+  }
+  else if(filteredExpenses.length>0)
+  {
+    expenseContent=filteredExpenses.map((data) => {
+      return (
+        <ExpenseItem
+          key={data.id}
+          date={data.date}
+          title={data.title}
+          location={data.location}
+          price={data.amount}
+        ></ExpenseItem>
+      );
+    })
+  }
 
   return (
     <Card className="expenses">
-    <ExpensesFilter fixedYear={filteredYear} year={getFilterYear} ></ExpensesFilter>
-      {filteredExpenses.map((data) => {
+      <ExpensesFilter
+        fixedYear={filteredYear}
+        year={getFilterYear}
+      ></ExpensesFilter>
+
+    {expenseContent}
+
+      {/* {filteredExpenses.length===0 && <p>No Expense Available</p> }
+      {filteredExpenses.length>0 && filteredExpenses.map((data) => {
         return (
           <ExpenseItem
-          key={data.id}
+            key={data.id}
             date={data.date}
             title={data.title}
             location={data.location}
             price={data.amount}
           ></ExpenseItem>
         );
-      })}
+      })} */}
+
+      {/* {filteredExpenses.length===0?<p>No Expense Available</p>:  filteredExpenses.map((data) => {
+        return (
+          <ExpenseItem
+            key={data.id}
+            date={data.date}
+            title={data.title}
+            location={data.location}
+            price={data.amount}
+          ></ExpenseItem>
+        );
+      })} */}
+      
     </Card>
   );
-}
+};
 
 export default Expense;
 
+//Condition rendering:
+//from line-> 59 to 69, we are using ternary operator simply, if length=0, paragrah will print otherwise map() will executed
 
+// From line --> 46 to 57 we are using "Absuing" here if condition before "&&" will satisfied, then data after "&&" will executed
 
-//JUST CHECKING
+//From line--> 21 to 39, we are using simple if statement of JS, by using this, return of function become clean
